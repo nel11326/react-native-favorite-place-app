d1ed420de56b3ed62a5f6eed66256567fb701af8
nel11326 <lenor11326@hotmail.com>
2024-08-29 07:46:20 +0800

    database(update): insert data to the database

---
+++ util/database.js
@@ -0,0 +1,52 @@
+import * as SQLite from "expo-sqlite";
+
+const database = SQLite.openDatabaseAsync("places.db");
+
+export function init() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
+                id INTEGER PRIMARY KEY NOT NULL,
+                title TEXT NOT NULL,
+                image TEXT NOT NULL,
+                address TEXT NOT NULL,
+                lat REAL NOT NULL,
+                lng REAL NOT NULL
+            )`,
+        [],
+        () => {
+          resolve();
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
+
+export function insertPlace(place) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, image, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.image,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
